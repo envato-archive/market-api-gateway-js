5a34ecfcdee7dd10a523342dadf5bdf70a7c77c8
fraserxu <xvfeng123@gmail.com>
2016-06-20 13:34:15 +1000

    add test for apiGateway instance

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,51 @@
 const test = require('tape')
+const noop = function () {}
 
 const ApiGateway = require('../lib/')
+const stats = require('../lib/stats')
+const catalog = require('../lib/catalog')
 
 test('new ApiGateway instance', function (t) {
-  const apiGateway = new ApiGateway('xxxxxxxx')
+  t.test('should throw if missing access token', function (assert) {
+    assert.throws(function () {
+      const apiGateway = new ApiGateway()
+      // do nothing but prevent standard complain about unused variable
+      noop(apiGateway)
+    }, 'shoud throw')
 
-  t.equal(typeof apiGateway, 'object', 'shoud be an object')
+    assert.throws(function () {
+      const config = {}
+      const apiGateway = new ApiGateway(config)
+      noop(apiGateway)
+    }, 'should throw if object missing token')
+    assert.end()
+  })
+
+  t.test('should create apiGateway instance with string', function (assert) {
+    const apiGateway = new ApiGateway('dummy token')
+
+    assert.equal(apiGateway instanceof ApiGateway, true, 'should be instance of ApiGateway')
+    assert.equal(typeof apiGateway._request, 'object', 'should have _request method')
+
+    assert.equal(apiGateway.stats instanceof stats, true, 'should create new stats instance')
+    assert.equal(apiGateway.catalog instanceof catalog, true, 'should create new catalog instance')
+
+    assert.end()
+  })
+
+  t.test('should create apiGateway instance with object', function (assert) {
+    const apiGateway = new ApiGateway({
+      ACCESS_TOKEN: 'dummy token'
+    })
+
+    assert.equal(apiGateway instanceof ApiGateway, true, 'should be instance of ApiGateway')
+    assert.equal(typeof apiGateway._request, 'object', 'should have _request method')
+
+    assert.equal(apiGateway.stats instanceof stats, true, 'should create new stats instance')
+    assert.equal(apiGateway.catalog instanceof catalog, true, 'should create new catalog instance')
+
+    assert.end()
+  })
 
   t.end()
 })
